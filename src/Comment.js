a57e654e88003ab1b15e5a3b44358fb92fdeb4e9
cejimenez0123 <christianjimenez0123@gmail.com>
2020-03-18 11:04:38 -0400

    Done.

--- src/Comment.js
+++ src/Comment.js
@@ -1,1 +1,12 @@
+import React, { Component } from "react";
+import BlogPost from "./BlogPost";
 //your code here
+ class Comment extends Component{
+     render(){
+         return (
+            <div class="comment" className="comment">{this.props.commentText}</div>
+         )
+     }
+
+ }
+ export default Comment
